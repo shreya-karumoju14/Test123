86cf4863bf003eac4b39835f735d4f082c6b2900
Karumoju Shreya <2020wa86710@wilp.bits-pilani.ac.in>
2024-04-25 00:43:24 +0530

    adding same function from main branch

--- file_1.c
+++ file_1.c
@@ -12,21 +12,4 @@ printf("%d +%d = %d, number1, number2, sum);
 return 0;
 }
 
-<<<<<<<<HEAD
-
-int addfunc(int a, int b)
-{
-   int c =a+b;
-   return c;
-=======
-//function added in branch1
-int func_add(int a, int b)
-{
-    return a+b;
->>>>>>>>>branch1
-}
-
-
-
-
 
