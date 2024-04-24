d1260497a44954474221cd382048131a680185ce
shreya-karumoju14 <163722277+shreya-karumoju14@users.noreply.github.com>
2024-04-24 23:17:48 +0530

    file_1.c

---
+++ file_1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+    
+      int number1, number2,sum;
+     
+      printf("Enter two integers:");
+      scanf("%d %d", &number1, &number2);
+    
+     //calculate the sum
+      sum = number1 + number 2;
+printf("%d +%d = %d, number1, number2, sum);
+return 0;
+}
+
+<<<<<<<<HEAD
+
+int addfunc(int a, int b)
+{
+   int c =a+b;
+   return c;
+=======
+//function added in branch1
+int func_add(int a, int b)
+{
+    return a+b;
+>>>>>>>branch1
+}
